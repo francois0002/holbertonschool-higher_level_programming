e742925cadf1a4f59bf058fea2302b6934856505
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-23 14:13:39 +0100

    commit task 1 to 7

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for a in range(97, 123):
+    a = chr(a)
+    print(f"{a}", end="")
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(97, 123):
+    if a == 101 or a == 113:
+        continue
+
+    a = chr(a)
+    print(f"{a}", end="")
