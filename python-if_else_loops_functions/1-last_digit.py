bb68a6be9f7509887d732bd8e4c4e508e064bc4b
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-23 14:45:33 +0100

    test coommit format

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,9 +9,9 @@ else:
 
 print(last_digit)
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print("Last digit of" ,number, "is" ,last_digit, "and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print("Last digit of" ,number, "is" ,last_digit, "and is 0")
 elif last_digit < 6:
-    print(f"Last digit of {number} is {last_digit} and is less \
+    print("Last digit of" ,number, "is" ,last_digit, "and is less \
 than 6 and not 0")
--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(0, 99):
     print("{:d} = 0x{:x}".format(x, x,))
-    #print(f"{x} = {hex(x)}")
+"""print(f"{x} = {hex(x)}")"""
