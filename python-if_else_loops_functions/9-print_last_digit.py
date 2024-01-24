d3197cb26999e4f80a12a5f4e0f4d0da9e77bcd4
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-24 10:05:41 +0100

    commit print last digit function

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number >= 0:
+        print(number % 10)
+        last_digit = number % 10
+        return last_digit
+    else:
+        number = number * -1
+        print(number % 10)
+        last_digit = number % 10
+        return last_digit
