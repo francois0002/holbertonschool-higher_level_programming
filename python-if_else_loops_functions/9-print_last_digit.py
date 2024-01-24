af7acf704095bf076b2cc28e59ad68cebd05ef33
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-24 10:17:05 +0100

    removespace with end=""

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
+
     if number >= 0:
-        print(number % 10)
+        print(number % 10, end="")
         last_digit = number % 10
         return last_digit
     else:
         number = number * -1
-        print(number % 10)
+        print(number % 10, end="")
         last_digit = number % 10
         return last_digit
+
