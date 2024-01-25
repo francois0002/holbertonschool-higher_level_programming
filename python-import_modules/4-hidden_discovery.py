171e864d01e4c021439fa7ca6a61522f632114d4
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-25 16:34:34 +0100

    pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    module_name= dir(hidden_4)
+    module_name = dir(hidden_4)
 
     for i in range(len(module_name)):
         if module_name[i][:2] != '__':
