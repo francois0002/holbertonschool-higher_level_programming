c22deab592e2d36f98c7bee647cf79fc440b8b39
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-25 16:33:23 +0100

    hidden discovery

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    module_name= dir(hidden_4)
+
+    for i in range(len(module_name)):
+        if module_name[i][:2] != '__':
+            print(module_name[i])
