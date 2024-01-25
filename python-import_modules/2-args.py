082f182fca4a548a9e4fe384a4e3ca459d0cc09d
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-25 15:43:01 +0100

    clean code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv
 
 if __name__ == "__main__":
 
     x = 1
 
-    if len(sys.argv) == 1:
-        print("{:d} arguments".format(len(sys.argv) - 1))
+    if len(argv) == 1:
+        print("0 arguments")
 
-    elif len(sys.argv) == 2:
-        print("{:d} argument".format(len(sys.argv) - 1))
-        print("{:d}: {}".format(x, sys.argv[x]))
+    elif len(argv) == 2:
+        print("1 argument")
+        print("{}: {}".format(x, argv[x]))
 
     else:
 
-        print("{} arguments".format(len(sys.argv) - 1))
-        while x < len(sys.argv):
-            print("{:d}: {}".format(x, sys.argv[x]))
+        print("{} arguments".format(len(argv) - 1))
+        while x < len(argv):
+            print("{}: {}".format(x, argv[x]))
             x = x + 1
