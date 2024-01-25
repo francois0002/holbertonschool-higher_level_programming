9761359746ffb19e5961dd9ddfdbcef175da0abd
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-25 15:34:49 +0100

    decimal in format args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     x = 1
 
     if len(sys.argv) == 1:
-        print("{} arguments".format(len(sys.argv) - 1))
+        print("{:d} arguments".format(len(sys.argv) - 1))
 
     elif len(sys.argv) == 2:
-        print("{} argument".format(len(sys.argv) - 1))
-        print("{}: {}".format(x, sys.argv[x]))
+        print("{:d} argument".format(len(sys.argv) - 1))
+        print("{:d}: {}".format(x, sys.argv[x]))
 
     else:
 
         print("{} arguments".format(len(sys.argv) - 1))
         while x < len(sys.argv):
-            print("{}: {}".format(x, sys.argv[x]))
+            print("{:d}: {}".format(x, sys.argv[x]))
             x = x + 1
