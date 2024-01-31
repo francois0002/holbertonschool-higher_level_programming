98b0baa142311f92619d3412880b3bf194dace8a
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-31 16:44:42 +0100

    pycodestyle task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        number = int(value)
-        print("{:d}".format(number))
+        print("{:d}".format(value))
 
-    except():
+    except:
         return False
 
     else:
---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+
+    except(ZeroDivisionError, ValueError):
+        pass
+
+    except():
+        pass
+
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+
