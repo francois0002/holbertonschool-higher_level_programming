a328bae258f6cb50f1073702c758d2aff3115e9a
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-31 16:22:50 +0100

    commit task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    index = 0
+    count = 0
+    try:
+        while index < x:
+            if isinstance(my_list[index], int):
+                print("{}".format(my_list[index]), end="")
+                index += 1
+                count += 1
+            else:
+                index += 1
+
+    except IndexError:
+        pass
+    except:
+        pass
+    print()
+    return count
