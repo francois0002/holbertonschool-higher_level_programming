2b4a0b4f36bc4267e74374c496f8146ef3241e60
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-31 16:57:39 +0100

    test delete except task3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,9 +6,6 @@ def safe_print_division(a, b):
     except(ZeroDivisionError, ValueError):
         return None
 
-    except:
-        return None
-
     else:
         return result
 
@@ -17,5 +14,3 @@ def safe_print_division(a, b):
             print("Inside result: {}".format(None))
         else:
             print("Inside result: {}".format(result))
-
-
