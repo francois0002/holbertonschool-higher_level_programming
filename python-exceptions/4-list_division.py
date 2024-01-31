244d0e32db77c8d8e34c05c46849f020faa83622
francois0002 <francois.gosselin.lille@gmail.com>
2024-01-31 18:08:44 +0100

    change indentation divise list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,28 +3,26 @@
 
 def list_division(my_list_1, my_list_2, list_length):
 
-        new_list = []
-        index = 0
-        while index < list_length:
-            try:
-                result = my_list_1[index] / my_list_2[index]
+    new_list = []
+    index = 0
+    while index < list_length:
+        try:
+            result = my_list_1[index] / my_list_2[index]
 
-            except IndexError:
-                  print("out of range")
-                  result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
 
-            except ZeroDivisionError:
-                 print("division by 0")
-                 result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
 
-            except TypeError:
-                 print("wrong type")
-                 result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
 
-            except:
-                 result = 0
-
+        finally:
             new_list.append(result)
             index += 1
 
-        return new_list
+    return new_list
