b1ae7a5409f7bf990ce05516a5b810389ae25e53
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-06 01:01:56 +0100

    push 5 tasks

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Compute and return the square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ Print a square with the character #"""
+        if self.__size == 0:
+            print("")
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        """ get & set the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+def complex_delete(a_dictionary, value):
+    for a_dictionary.values in a_dictionary:
+        if a_dictionary.values == value:
+            del a_dictionary.values
+        else:
+            continue
