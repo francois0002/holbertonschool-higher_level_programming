8021b2ecfac571b161cc621be425fe952ecb5b27
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-07 18:05:01 +0100

    commit 7 task

---
+++ python-more_classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module initialize a class named Square"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Compute and return the square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ get & set the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
