cd90018bab9a824ee44c70a3dfced16907988b7e
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-10 12:40:28 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-"""This module creates an empty class named Square"""
+"""This module add 2 numbers
+
+This module make a operation an compute two numbers,
+these numbers can be integers or floats.
+
+"""
 
 
 def add_integer(a, b=98):
+    """Add 2 integers
+    Args:
+        a (:obj:`int, float`): The first number.
+        b (:obj:`int, float`, optional): The second number.
+
+        Return : (int) the result of the addition
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
@@ -13,4 +25,8 @@ def add_integer(a, b=98):
         int(b)
     return a + b
 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
