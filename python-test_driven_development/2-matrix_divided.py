b9a1d1bf705868e871071e07c943dd1b6ffba60f
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-11 09:46:09 +0100

    task matrice

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""This module add 2 numbers
-
-This module make a operation an compute two numbers,
-these numbers can be integers or floats.
-
 """
-
-
-def add_integer(a, b=98):
-    """Add 2 integers
-    Args:
-        a (:obj:`int, float`): The first number.
-        b (:obj:`int, float`, optional): The second number.
-
-        Return : (int) the result of the addition
+Module Devide Matrix
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    if isinstance(a, float) is True:
-        int(a)
-    if isinstance(b, float) is True:
-        int(b)
-    return a + b
 
 
+def matrix_divided(matrix, div):
+    """
+        Devides all elements in matrix
+
+        Args:
+            matrix (list[list[int/float]]) : matrice
+            div (int/float) Devider
+
+        Raise:
+            TypeError: div not int or float
+            TypeError: matix is not a list of list of number
+            ZeroDivisionError: Div is 0
+
+        Return : New matrix Devided
+
+        """
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if type(matrix) is not list or not all((type(l) is list)for l in matrix) \
+        or not all((isinstance(n, (int, float))for n in l)for l in matrix) \
+            or len(matrix[0]) == 0:
+        raise TypeError(
+                "matrix must be a matrix "
+                "(list of lists) of integers/floats")
+    l = len(matrix[0])
+    if not all((len(x) == l)for x in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    return [list(map(lambda x: round(x / div, 2), r))for r in matrix]
