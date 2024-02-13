30faea7ed2b170c7ed19769a8cdcd6de7b747491
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-13 14:48:32 +0100

    correction code

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,8 +6,4 @@ class MyList(list):
     """print the list named List sorted"""
     def print_sorted(self):
         """print the list named List sorted"""
-        if MyList is not None:
-            sorted_list = sorted(self)
-            print("{}".format(sorted_list))
-        else:
-            return None
+        print(sorted(self))
