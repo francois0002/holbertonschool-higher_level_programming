e3153b1739c5b469c84e3d2b15a8f9178f533f9f
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-13 14:46:32 +0100

    test files

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -3,5 +3,5 @@ MyList = __import__('1-my_list').MyList
 
 my_list = None
 print(my_list)
-my_list.print_sorted()
+my_list.print_sorted(test)
 print(my_list)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,7 @@
 class MyList(list):
     """print the list named List sorted"""
     def print_sorted(self):
+        """print the list named List sorted"""
         if MyList is not None:
             sorted_list = sorted(self)
             print("{}".format(sorted_list))
