d720badaef47be6a2cb2b6d9c5af389b7a7c6334
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-15 15:04:19 +0100

    task 2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,50 +1,11 @@
 #!/usr/bin/python3
-"""This module creates an empty class named Rectangle"""
+""" Module  writes a string to a text file (UTF8) and
+returns the number of characters written"""
 
 
-class Rectangle:
-    """Define a rectangle"""
-    def __init__(self, width=0, height=0):
-        """Initialize a new rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle
-        """
-        if isinstance(width, int) is False:
-            raise TypeError("width must be an integer")
-        if width < 0 or height < 0:
-            raise ValueError("width must be >= 0")
-        if isinstance(height, int) is False:
-            raise TypeError("height must be an integer")
-        if height < 0 or height < 0:
-            raise ValueError("height must be >= 0")
-
-        self.__height = height
-        self.__width = width
-
-    @property
-    def height(self):
-        """ get & set the width of the rectangle """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if isinstance(value, int) is False:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @property
-    def width(self):
-        """ get & set the width of the rectangle """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if isinstance(value, int) is False:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+def write_file(filename="", text=""):
+    """ Writes a string to a text file (UTF8) and
+        returns the number of characters written"""
+    with open(filename, "w") as file:
+        file.write(text)
+        return len(text)
