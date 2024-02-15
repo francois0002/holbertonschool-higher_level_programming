467f5d10225b82bb9eccbd504610332b8f5ab9a7
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-15 15:38:16 +0100

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
-"""This module initialize a class named Square"""
+"""Module that writes an Object to a text file,
+using a JSON representation"""
+
+import json
 
 
-class Square:
-    """Define a square"""
-    def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        if isinstance(size, int) is False:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """ Compute and return the square area"""
-        return self.__size * self.__size
-
-    @property
-    def size(self):
-        """ get & set the size of the square """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if isinstance(value, int) is False:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file, using a JSON representation"""
+    json_content = json.dumps(my_obj)
+    with open(filename, "w") as file:
+        file.write(json_content)
