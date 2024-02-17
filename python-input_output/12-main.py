ae269b70537571c1f6224f64a559b6ddb63a5521
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-17 14:36:30 +0100

    task 12

--- python-input_output/12-main.py
+++ python-input_output/12-main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""
-12-main
-"""
-pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
+"""Defines a Pascal's Triangle function."""
 
-def print_triangle(triangle):
+
+def pascal_triangle(n):
     """
-    Print the triangle
+    Returns a list of lists of integers representing the triangle.
     """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+    if n <= 0:
+        return []
 
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
