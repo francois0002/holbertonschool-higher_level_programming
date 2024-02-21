df261d26e98584198fc579aa396adf25125cd7aa
francois0002 <francois.gosselin.lille@gmail.com>
2024-02-21 11:34:03 +0100

    test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,12 +2,45 @@
 """Defines unittests for base.py"""
 
 
-import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
-class TestBase_creation_class(unittest.TestCase):
-    """Unittests for testing creation of the Base class."""
+
+class TestBase(unittest.TestCase):
+    """test on testbase"""
+
+
+    def test_init_with_id(self):
+        instance = Base(id=5)
+        self.assertEqual(instance.id, 5)
+
+    def test_init_without_id(self):
+        instance = Base()
+        self.assertEqual(instance.id, 1)
+
+    def test_to_json_string(self):
+        instance = Base()
+        result = instance.to_json_string([{'key': 'value'}])
+        self.assertEqual(result, '[{"key": "value"}]')
+
+    def test_to_json_string_empty_list(self):
+        instance = Base()
+        result = instance.to_json_string([])
+        self.assertEqual(result, '[]')
+
+    def test_save_to_file(self):
+        instance = Base()
+        instance.save_to_file([instance])
+
+
+    def test_from_json_string(self):
+        instance = Base()
+        result = instance.from_json_string('[{"key": "value"}]')
+        self.assertEqual(result, [{'key': 'value'}])
+
+
+if __name__ == '__main__':
+    unittest.main()
