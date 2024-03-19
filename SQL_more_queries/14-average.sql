d5cf6419156193006dde11bc22f104d7f483f3f8
francois0002 <francois.gosselin.lille@gmail.com>
2024-03-19 15:46:14 +0100

    task 15

--- SQL_more_queries/14-average.sql
+++
@@ -1,3 +0,0 @@
--- computes the score average
--- all records in the table second_table of the database hbtn_0c_0
-SELECT AVG(score) 'average' FROM second_table;
--- SQL_more_queries/8-count_89.sql
+++
@@ -1,3 +0,0 @@
--- displays the number of records
--- database hbtn_0c_0
-SELECT COUNT(id) FROM first_table WHERE ID = 89;
