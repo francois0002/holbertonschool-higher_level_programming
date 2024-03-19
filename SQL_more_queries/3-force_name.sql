0b095b4a56e3a748ac6745d90cccc6acadfabdd3
francois0002 <francois.gosselin.lille@gmail.com>
2024-03-19 11:23:06 +0100

    TASK 5

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- creates the table force_name
+-- ID & NAME
+CREATE TABLE IF NOT EXISTS force_name
+(
+    `id` INT,
+    `name` VARCHAR(256) NOT NULL
+);
+
---
+++ SQL_more_queries/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records
+-- database hbtn_0c_0
+SELECT COUNT(id) FROM first_table WHERE ID = 89;
